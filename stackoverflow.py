0c365801c5c0bf2b2e3e5591e0f00af1d16d73f9
AhnDogeon <qltiqlti@gmail.com>
2021-01-25 21:55:43 +0900

    20210125 | StackOverFlow extract Job Title, Company, Location

--- stackoverflow.py
+++ stackoverflow.py
@@ -10,6 +10,15 @@ def get_last_page():
     last_page = pages[-2].get_text(strip=True)
     return int(last_page)
 
+def extract_job(html):
+    title = html.find("h2", {"class": "mb4"}).find("a")["title"]
+    # recursive는 span 안에 span 이 있을 경우 계속 타고 들어가지 않게 한다. (제일 위에 것만 가져옴)
+    company, location = html.find("h3", {"class": "fc-black-700"}).find_all("span", recursive = False)
+    company, location = company.get_text(strip=True), location.get_text(strip=True)
+    print(company)
+    print(location)
+    return {'title' : title}
+
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
@@ -17,7 +26,9 @@ def extract_jobs(last_page):
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class" : "-job"})
         for result in results:
-            print(result["data-jobid"])
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
 
 def get_jobs():
