a96c35324638b4c44e71d909fb88ae2ce20a9bea
AhnDogeon <qltiqlti@gmail.com>
2021-01-25 21:37:16 +0900

    20210125 | StackOverFlow extract jobs

--- stackoverflow.py
+++ stackoverflow.py
@@ -7,8 +7,20 @@ def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", class_ = "s-pagination").find_all("a")
-    print(pages)
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&pg={page + 1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class" : "-job"})
+        for result in results:
+            print(result["data-jobid"])
+
 
 def get_jobs():
     last_page = get_last_page()
-    return []
+    jobs = extract_jobs(last_page)
+    return jobs
