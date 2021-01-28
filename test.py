c8e4941ad7dd0e54c36926fb3dedc36e1a6e7d07
AhnDogeon <qltiqlti@gmail.com>
2021-01-28 22:49:59 +0900

    20210128 | test

---
+++ test.py
@@ -0,0 +1,17 @@
+import requests
+
+
+while (True):
+  print("please write URL")
+  URLs = list(map(str(input().split())))
+  print(URLs)
+  URL = str(input())
+  if ('https://' not in URL):
+    URL = 'https://' + URL
+  if ('com' not in URL):
+      URL = URL + 'com'
+  response = requests.get(URL)
+  if (response.status_code == 200):
+    print(f'{URL} is UP!')
+  else:
+    print(f'{URL} is Down!')
