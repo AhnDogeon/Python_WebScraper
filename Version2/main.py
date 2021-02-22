25d601a0ab8f654a4a5afcbd0e20f4bce750e97b
AhnDogeon <qltiqlti@gmail.com>
2021-02-22 22:44:53 +0900

    210222 | Flask

--- Version2/main.py
+++ Version2/main.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask("SuperScrapper")
 
+# @는 Decorator -> 바로 아래에 있는 함수만 본다!
+
 @app.route("/")
 def home():
     return "Hello Welcome!"
-
+# / 아래 주소명과 함수명이 일치할 필요는 없다.
 @app.route("/contact")
 def contact():
     return "contact me!"
